d5cede8e6d948205febf9dd82c77fd5db0a808f9
omh1004 <ohchef2020@gmail.com>
2025-02-16 21:00:29 +0900

    helloMvc boot init

---
+++ src/main/java/com/mh/hellobootmvc/HellobootMvcApplication.java
@@ -0,0 +1,13 @@
+package com.mh.hellobootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobootMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellobootMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mh/hellobootmvc/HellobootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mh.hellobootmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellobootMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
